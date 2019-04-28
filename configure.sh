527c378749c2d1784583ce343fe39b5e73a82055
Herolane <admin@alexanderheld.net>
2019-04-28 09:10:35 +0200

    Adjust Configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+
+
+# Install brew
+# Install brew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+
+fi
+
 # Update Brew
 echo "Updating package lists..."
 brew update
