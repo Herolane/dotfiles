b8e5808f9632c9005ad6f28b300ad3d485232be6
Herolane <45519084+Herolane@users.noreply.github.com>
2019-04-28 08:23:13 +0200

    Delete homebrew.zsh

--- homebrew/homebrew.zsh
+++
@@ -1,6 +0,0 @@
-# To opt out of Homebrew's analytics
-# HOMEBREW_NO_ANALYTICS=1
-
-# You may also wish to enable additional security options
-# HOMEBREW_NO_INSECURE_REDIRECT=1
-# HOMEBREW_CASK_OPTS=--require-sha
