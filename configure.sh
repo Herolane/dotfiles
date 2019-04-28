daea6087a00ee715874aa504ce7fda57fbfea49a
Herolane <45519084+Herolane@users.noreply.github.com>
2019-04-28 07:23:26 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Updating package lists..."
 brew update
